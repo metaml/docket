5c3a57acf8d0f94d7352e5528597aa183f276057
ml <metaml@gmail.com>
2016-03-24 17:42:50 -0400

    added target got add docker group and add user to it

--- mk/docker.mk
+++ mk/docker.mk
@@ -2,9 +2,9 @@
 
 .DEFAULT_GOAL: preamble ppa update docker docker-compose
 
-docker:; sudo apt-get install -y docker-engine
+docker:; apt-get install -y docker-engine
 
-docker-compose:; sudo apt-get install -y docker-compose
+docker-compose:; apt-get install -y docker-compose
 
 update:; apt-get update -y && apt-get upgrade -y
 
@@ -12,15 +12,16 @@ ppa:; add-apt-repository -y ppa:webupd8team/java
 
 CODENAME := $(shell lsb_release --codename | awk '{print $$NF}')
 preamble:
-	sudo apt-get update -y
-	sudo apt-get install -y apt-transport-https ca-certificates
-	sudo apt-key adv \
+	apt-get update -y
+	apt-get install -y apt-transport-https ca-certificates
+	apt-key adv \
 		--keyserver hkp://p80.pool.sks-keyservers.net:80 \
 		--recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 	# @todo: may want to ask for yay/nay before proceeding
 	echo deb https://apt.dockerproject.org/repo ubuntu-${CODENAME} main \
-	| sudo tee /etc/apt/sources.list.d/docker.list
-	sudo apt-get update -y
-	sudo apt-get install -y linux-image-extra-$$(uname -r)
+	| tee /etc/apt/sources.list.d/docker.list
+	apt-get update -y
+	apt-get install -y linux-image-extra-$$(uname -r)
 
-codename:; echo ${CODENAME}
+docker-group:
+	[ -z "$$LOGNAME" ] || usermod -aG docker $$LOGNAME
