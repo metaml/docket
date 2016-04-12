e00ecf3b20c511929ed9598c3bedfd44703d1dd3
ml <metaml@gmail.com>
2016-04-11 22:01:51 -0400

    updated to v. 0.12

--- mk/influxdb.mk
+++ mk/influxdb.mk
@@ -5,15 +5,14 @@ PATH := /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 DEBIAN_FRONTEND := noninteractive
 
-INFLUX_VER = 0.11.1-1
+INFLUX_VER = 0.12.1-1
 INFLUX_URL = https://s3.amazonaws.com/influxdb/influxdb-${INFLUX_VER}_linux_amd64.tar.gz
 install: influxdb chown
 
 OPT ?= /opt
 influxdb:
-	mkdir -p ${OPT}
-	cd ${OPT} && wget --quiet --output-document=- ${INFLUX_URL} | tar xz
-	cd ${OPT} && mv -f influxdb-${INFLUX_VER} influxdb
+	mkdir -p ${OPT}/influxdb
+	cd ${OPT}/influxdb && wget --quiet --output-document=- ${INFLUX_URL} | tar xz
 	mkdir -p ${OPT}/influxdb/meta
 
 chown:; chown -R root:root ${OPT}/influxdb
