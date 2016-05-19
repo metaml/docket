2041c9a597794293d8976859741652387857d3e5
ml <metaml@gmail.com>
2016-05-19 16:45:05 -0400

    bug fixes

--- mk/zinc.mk
+++ mk/zinc.mk
@@ -3,12 +3,11 @@ ZINC_URL = http://downloads.typesafe.com/zinc/${ZINC_VER}/zinc-${ZINC_VER}.tgz
 ZINC_TGZ := $(shell echo ${ZINC_URL} | awk -F/ '{print $$NF}')
 
 zinc: ## install zinc
-	[ -d /opt ] || sudo mkdir -p /opt
+	[ -d /opt ] ||  mkdir -p /opt
 	if [ ! -d /opt/$@/bin ]; then \
 		cd /var/tmp \
 		&& wget ${ZINC_URL} \
 		&& tar xvf ${ZINC_TGZ} \
-		&& echo sudo neede here: \
-		&& sudo mv -f /var/tmp/zinc-${ZINC_VER} /opt/$@ \
+		&& mv -f /var/tmp/zinc-${ZINC_VER} /opt/$@ \
 		&& echo installed $@ in /opt/$@; \
 	fi
