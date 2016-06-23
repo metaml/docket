6c50123549586dd70d375b30ac0f565cc853f2a5
ml <metaml@gmail.com>
2016-06-23 11:07:54 -0400

    spark

---
+++ mk/spark.mk
@@ -0,0 +1,15 @@
+VER = 1.6.1
+URL = http://d3kbcqa49mib13.cloudfront.net/spark-${VER}-bin-without-hadoop.tgz
+TGZ = $(shell echo ${URL} | awk -F/ '{print $$NF}')
+DIR = $(subst .tgz,,${TGZ})
+
+spark: get-spark mkdir
+	mv -f /var/tmp/${DIR}/* /opt/spark/.
+
+mkdir:;	mkdir -p /opt/spark
+
+get-spark:
+	cd /var/tmp && curl ${URL} > ${TGZ}
+	cd /var/tmp && tar xzf ${TGZ} && rm -f ${TGZ}
+
+.PHONY: spark mkdir get-spark
